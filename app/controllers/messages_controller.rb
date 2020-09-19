94b034a933cccb80df76cca8802a011d7eb118cd
Franz <Franz@LAPTOP-JC7PJSNR>
2020-09-19 10:40:10 +0800

    finished sending a message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,12 +5,18 @@ class MessagesController < ApplicationController
         @current_user_id = current_user.id
         @message = Message.new(message_params)
         @message.user_id = @current_user_id
-        @message.save
+        if @message.save
+            ActionCable.server.broadcast "chatroom_channel", mod_message: message_render(@message)
+        end
     end
 
     private
 
     def message_params
         params.require(:message).permit(:message_body, :user_id)
+    end
+
+    def message_render(message)
+        render(partial: 'message', locals: {message: message})
     end
 end
